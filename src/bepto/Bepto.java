c9778f8e0a544767780d39c82d4cacef480440b3
Andrés Arnedo <andresarnedo2002@gmail.com>
2022-08-16 08:46:37 -0500

    Actualización en la lectura

--- src/bepto/Bepto.java
+++ src/bepto/Bepto.java
@@ -7,6 +7,8 @@ package bepto;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  *
@@ -19,21 +21,30 @@ public class Bepto {
      */
     public static void main(String[] args) {
 
+        Path path = Paths.get("src/File/2S_FSJ.b2pFloorMapReport.txt");
         String linea = "";
         String delimitante = ";";
         try {
-            String filePath = "C:\\Users\\jaug1\\Downloads\\2S_FSJ.b2pFloorMapReport.txt";
+            String filePath = path.toString();
             FileReader fileReader = new FileReader(filePath);
             BufferedReader reader = new BufferedReader(fileReader);
             String[] campo;
 
             while ((linea = reader.readLine()) != null) {
+                int a = 0;
                 if (linea.contains(delimitante)) {
                     campo = linea.split(delimitante);
                     for (String data : campo) {
                         System.out.print(data + " ");
                     }
                     System.out.println("");
+                } else if (linea.toLowerCase().contains("route")) {
+                    campo = linea.split(" ");
+                    for (String data : campo) {
+                        a++;
+                        System.out.println(data + " " + a);
+                    }
+                    break;
                 }
 
             }
